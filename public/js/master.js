08eca50a57bd02ef14541c5b8b4c35f275e5533f
makmm <makmmelmejorenlacompu@gmail.com>
2016-06-17 09:38:09 -0300

    fix delete

--- public/js/master.js
+++ public/js/master.js
@@ -11,12 +11,34 @@ function isEmpty(str) {
 function send(){
 	var nombre = $("#nombre").val();
 	var descripcion = $("#descripcion").val();
-	if(!isEmpty(nombre)) {
-		productos.push({"nombre": nombre, "descripcion": descripcion});
-		$("#warning").html(alertTemplate({type: "success", title: "Bien!", message: "Producto insertado correctamente."}));
+	var check = false;
+	if (productos.length<1){
+		check = true;
+	} else {
+		for (var i = 0; i <= productos.length-1; i++) {
+			if (productos[i].nombre == nombre) {
+				if (check == false) check = false;
+			} else check = true;
+		};
+	};
+	if (check == false){
+		$("#warning").html(alertTemplate({type: "danger", title: "Error!", message: "El producto insertado tiene el mismo nombre que otro."}));
+	} else {
+		if (!isEmpty(nombre)) {
+			productos.push({"nombre": nombre, "descripcion": descripcion});
+			$("#warning").html(alertTemplate({type: "success", title: "Bien!", message: "Producto insertado correctamente."}));
+			update();
+		}
+		else $("#warning").html(alertTemplate({type: "danger", title: "Error!", message: "El nombre del producto que has puesto no significa nada."}));
 	}
-	else $("#warning").html(alertTemplate({type: "danger", title: "Error!", message: "El nombre del producto que has puesto no significa nada."}));
-	update();
+}
+
+function del(nombre){
+	for (var i = 0; i <= productos.length-1; i++) {
+		if (productos[i].nombre == nombre) {
+			productos.splice(i, 1);
+		}
+	};
 }
 
 function set(i){
@@ -34,7 +56,7 @@ function update(){
 	$("#lista ul").html("");
 	for (var i = 0; i <= productos.length-1; i++) {
 		$("body").append("<div id='myModal"+i+10+"' class='modal fade' role='dialog'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal'>&times;</button><h4 class='modal-title'>Edit product...</h4></div><div class='modal-body' id='add'><input id='nuevonombre"+i+10+"' class='form-control' value='"+productos[i].nombre+"' style='border-bottom-left-radius:0;border-bottom-right-radius:0;' placeholder='Name'><textarea id='nuevadescripcion"+i+10+"' class='form-control' style='border-top-left-radius:0;border-top-right-radius:0;' placeholder='Description'>"+productos[i].descripcion+"</textarea></div><div class='modal-footer'><button type='button' class='btn btn-warning' onclick=\"set("+i+");$(\'#myModal"+i+10+"\').modal(\'hide\');\">Edit</button></div></div></div></div>");
-		$("#lista ul").append("<li class='list-group-item'><div class='btn-group right' role='group' aria-label='...'><button type='button' class='btn btn-warning' onclick=\"$(\'#myModal"+i+10+"\').modal(\'show\');\">Edit</button><button type='button' class='btn btn-danger' onclick='productos.splice("+i+", 1);update();'>Delete</button></div><h3>"+productos[i].nombre+"</h3><p>"+productos[i].descripcion+"</p></li>");
+		$("#lista ul").append("<li class='list-group-item'><div class='btn-group right' role='group' aria-label='...'><button type='button' class='btn btn-warning' onclick=\"$(\'#myModal"+i+10+"\').modal(\'show\');\">Edit</button><button type='button' class='btn btn-danger' onclick=\"del(\'"+productos[i].nombre+"\');update();\">Delete</button></div><h3>"+productos[i].nombre+"</h3><p>"+productos[i].descripcion+"</p></li>");
 	}
 }
 
